7cafc303d4583f96e6f5784ce432b49af2f78fa6
Ruba24 <rubaqazi2000@gmail.com>
2025-07-12 14:28:30 +0500

    Customized ScrollBar Added

--- src/components/AwardsSlider.jsx
+++ src/components/AwardsSlider.jsx
@@ -3,31 +3,26 @@
 import React from 'react'
 
 const awards = [
-  {
-    image: '/images/awards/award1.png',
-    name: 'Top Amazon Partner',
-  },
-  {
-    image: '/images/awards/award2.png',
-    name: 'Best Shopify Agency',
-  },
-  {
-    image: '/images/awards/award3.png',
-    name: 'Meta Ads Certified',
-  },
-
+  { image: '/images/awards/award1.png', name: 'Top Amazon Partner' },
+  { image: '/images/awards/award2.png', name: 'Best Shopify Agency' },
+  { image: '/images/awards/award3.png', name: 'Meta Ads Certified' },
+  { image: '/images/awards/award4.png', name: 'Top Ecommerce Growth' },
+  { image: '/images/awards/award5.png', name: 'Client Satisfaction Award' },
 ]
 
 const AwardsSlider = () => {
+  // Repeat awards 3 times for seamless infinite scroll
+  const repeatedAwards = [...awards, ...awards, ...awards]
+
   return (
     <section className="bg-white py-10 overflow-hidden">
       <h2 className="text-3xl font-bold text-center text-[#1F102E] mb-8">
         Our <span className="text-[#B877F7]">Awards & Recognitions</span>
       </h2>
 
-      <div className="overflow-hidden w-screen relative left-[50%] right-[50%] -mx-[50vw]">
-        <div className="flex whitespace-nowrap animate-marquee">
-          {[...awards, ...awards].map((award, index) => (
+      <div className="overflow-hidden w-screen relative left-1/2 right-1/2 -mx-[50vw] group">
+        <div className="flex whitespace-nowrap animate-award-marquee">
+          {repeatedAwards.map((award, index) => (
             <div
               key={index}
               className="inline-flex flex-col items-center text-[#1F102E] w-[180px] px-4"
