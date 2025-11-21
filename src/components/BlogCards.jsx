f2417754226e7dea4a2597fc56b619a1776fae5c
Mac <apple@192.168.1.7>
2025-11-22 02:50:20 +0500

    fixes

--- src/components/BlogCards.jsx
+++ src/components/BlogCards.jsx
@@ -5,7 +5,8 @@ import Link from 'next/link'
 const BlogCards = ({ blogs }) => {
   
   return (
-    <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+    <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+
       {blogs.map((blog, index) => (
         <div
           key={index}
