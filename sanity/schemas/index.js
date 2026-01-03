94ce581e5e223c4cdc3eec63c3a59bbcd5b85c75
Mac <apple@192.168.1.3>
2026-01-03 20:07:52 +0500

    New Sanity Fixes

--- sanity/schemas/index.js
+++ sanity/schemas/index.js
@@ -1,14 +1,14 @@
 // schemas/index.js
 
 import service from './service'
-import teamMember from './teamMembers'
+import teamMember from './teamMember'
 import caseStudy from './caseStudy'
 import blog from './blog'
-import testimonial from './testimonials'
+import testimonial from './testimonial'
 import award from './award'
 import tool from './tool'
 import course from './course'
-import freeCourse from './freeCourses'
+import freeCourse from './freeCourse'
 import faq from './faq'
 import homepage from './homepage'
 
